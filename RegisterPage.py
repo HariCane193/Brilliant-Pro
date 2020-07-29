582af595705189535c3523ada19aaee06b8cd064
HariCane193 <srihari19cr@gmail.com>
2020-07-29 18:34:39 +0530

    Register Page created

---
+++ RegisterPage.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+
+main = Tk()
+main.title('Register')
+main.geometry('600x500')
+
+
+#Background
+b_img = ImageTk.PhotoImage(Image.open('Images/regbg.jpg'))
+b_l = Label(main,image = b_img)
+b_l.place(x = 0,y = 0)
+
+#Functions
+
+def confirm():
+    global main,user,passw
+    if C_pass.get()!=Password.get():
+        messagebox.showerror('Error',"Passwords don't match")
+        return
+    res = messagebox.askyesno('Confirmation','Are you sure?')
+    if res:
+        user = Username.get()
+        passw = Password.get()
+        main.quit()
+    else:
+        return
+
+
+#Entries
+
+Username = Entry(main,width = 50)
+Password = Entry(main,width = 50)
+Username.place(x = 170,y = 200)
+Password.place(x = 170,y = 230)
+
+C_pass = Entry(main,width = 50)
+C_pass.place(x = 170,y = 260)
+
+Username.insert(0,'Username')
+Password.insert(0,'Password')
+C_pass.insert(0,'Password')
+
+#Buttons
+
+Register = Button(main,text = 'REGISTER',font = ('Arial Black',13),command = confirm)
+Register.place(x = 250,y = 320)
+
+#Labels
+wel = Label(main,text = "Welcome to Brilliant Pro!",font = ('Freestyle Script',42))
+wel.place(x = 130,y = 100)
+
+U = Label(main,text = 'Username: ')
+p = Label(main,text = 'Password: ')
+c = Label(main,text = 'Confirm Password: ')
+
+U.place(x = 100,y = 197)
+p.place(x = 100,y = 227)
+c.place(x = 50,y = 260)
+main.mainloop()
+
