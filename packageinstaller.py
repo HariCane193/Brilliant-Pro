b22cfc6d9c8a481efc8eacd814201286b30c8672
HariCane193 <srihari19cr@gmail.com>
2021-02-12 12:58:04 +0530

    Package Installer

---
+++ packageinstaller.py
@@ -0,0 +1,7 @@
+import sys
+import subprocess
+
+#pip as subprocess
+subprocess.check_call([sys.executable,'-m','pip','install','instabot'])
+subprocess.check_call([sys.executable,'-m','pip','install','mysql-connector-python'])
+subprocess.check_call([sys.executable,'-m','pip','install','Pillow'])
