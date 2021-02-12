93599228166cb8a77c66e19895c1afe810f21f22
BlackDragon975 <68782904+BlackDragon975@users.noreply.github.com>
2021-02-12 16:27:29 +0530

    Update packageinstaller.py

--- packageinstaller.py
+++ packageinstaller.py
@@ -5,3 +5,4 @@ import subprocess
 subprocess.check_call([sys.executable,'-m','pip','install','instabot'])
 subprocess.check_call([sys.executable,'-m','pip','install','mysql-connector-python'])
 subprocess.check_call([sys.executable,'-m','pip','install','Pillow'])
+subprocess.check_call([sys.executable,'-m','pip','install','mathplot'])
